edc5b19f06487f769daa448c036b3fd2873038db
Sobirjon <ahmadjonovsobirjon24@gmail.com>
2025-05-13 16:58:37 +0500

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { ProfileModule } from './profile/profile.module';
+import { SubscriptionModule } from './subscription/subscription.module';
+import { MovieModule } from './movie/movie.module';
+import { FavoriteModule } from './favorite/favorite.module';
+import { ReviewModule } from './review/review.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ProfileModule, SubscriptionModule, MovieModule, FavoriteModule, ReviewModule, AdminModule],
   controllers: [],
   providers: [],
 })
