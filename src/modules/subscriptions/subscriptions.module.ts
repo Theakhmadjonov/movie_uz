30a551e2dc558694c2d7ecd16d8226e1043223e5
Sobirjon <ahmadjonovsobirjon24@gmail.com>
2025-05-13 21:09:05 +0500

    .

--- src/modules/subscriptions/subscriptions.module.ts
+++ src/modules/subscriptions/subscriptions.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SubscriptionsService } from './subscriptions.service';
 import { SubscriptionsController } from './subscriptions.controller';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
+  imports: [MulterModule.register({
+    dest: "/uploads"
+  })],
   controllers: [SubscriptionsController],
   providers: [SubscriptionsService],
 })
